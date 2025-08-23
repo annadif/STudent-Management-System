12d11aad12f46f7847a409d130607c0e55b0367b
annadif <128553523+annadif@users.noreply.github.com>
2025-08-24 00:02:49 +0300

    Reformat CourseFormm.cs with consistent spacing

--- CourseFormm.cs
+++ CourseFormm.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
+
 namespace Transparent_Form
 {
     public partial class CourseForm : Form
@@ -65,4 +66,5 @@ namespace Transparent_Form
         }
     }
 }
+
 
