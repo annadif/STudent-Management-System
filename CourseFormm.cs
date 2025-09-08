dc74b4fe548b0d9773ee5b061d1b6f8b9f53c402
annadif <128553523+annadif@users.noreply.github.com>
2025-09-08 15:42:22 +0300

    Update CourseFormm.cs

--- CourseFormm.cs
+++ CourseFormm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+using Sysstem.this.get
 
 namespace Transparent_Form
 {
@@ -66,6 +67,7 @@ namespace Transparent_Form
         }
     }
 }
+
 
 
 
