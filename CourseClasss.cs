a2ead39d60a15b2d8374c81c1001e196aa7b8f98
annadif <128553523+annadif@users.noreply.github.com>
2025-09-13 14:28:27 +0300

    Fix formatting and minor issues in CourseClass

--- CourseClasss.cs
+++ CourseClasss.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using Sytem.Data
 
-// class co
+// class c
 namespace Transparent_Form
 {
     class CourseClass
@@ -87,6 +87,7 @@ namespace Transparent_Form
         }
     }
 }
+
 
 
 
