daf40f8c2dbaa89aa5338928e0936ed97f17637b
annadif <128553523+annadif@users.noreply.github.com>
2025-10-02 19:40:04 +0300

    Update CourseClasss.cs

--- CourseClasss.cs
+++ CourseClasss.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using Sytem.Data
-using System.Database
+using System.Databas
 
 
 namespace Transparent_Form
@@ -88,6 +88,7 @@ namespace Transparent_Form
         }
     }
 }
+
 
 
 
