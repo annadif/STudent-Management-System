2f8edb74aeac0d73316627687ce44c8263d9bece
annadif <128553523+annadif@users.noreply.github.com>
2025-10-05 16:51:23 +0300

    Refactor StudentClass for improved readability

--- StudentClass.cs
+++ StudentClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Data;
 
-/./.mn
+/./.m
 namespace Transparent_Form
 {
     class StudentClass
@@ -143,6 +143,7 @@ namespace Transparent_Form
         }
     }
 }
+
 
 
 
